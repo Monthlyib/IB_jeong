b6bdca8e12e87f4763a8f729cbe4ddcf52a76b03
Mark Chang <wkdgnsgo@gmail.com>
2024-05-24 16:16:46 +0900

    updated again

---
+++ MonthlyIB/front/components/loginComponents/Login.jsx
@@ -0,0 +1,140 @@
+"use client";
+
+import styles from "./Login.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const googleLink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}.apps.googleusercontent.com&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}&response_type=token&scope=email profile`;
+
+const kakaoLink = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&response_type=code`;
+
+export const naverLink = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&state=dijDIJFDIdk&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URL}`;
+
+function Login() {
+  const [username, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  useEffect(() => {
+    if (session?.userStatus === "ACTIVE") {
+      router.replace("/");
+    }
+  }, [session]);
+
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const onSubmitForm = useCallback(
+    async (e) => {
+      e.preventDefault();
+      try {
+        const res = await signIn("credentials", {
+          username,
+          password,
+          redirect: false,
+        });
+        if (!res.ok && res?.status === 401) {
+          alert("아이디/비밀번호가 틀렸거나 존재하지 않는 회원입니다.");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [username, password]
+  );
+
+  return (
+    <>
+      <main
+        className={`${styles.width_content} ${styles.min_content} ${styles.login} `}
+      >
+        <div className={`${styles.header_tit_wrap} ${styles.tit_center} `}>
+          <span>Member Login</span>
+          <h2>로그인</h2>
+        </div>
+        <form onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            name="user-id"
+            value={username || ""}
+            onChange={onChangeId}
+            required
+            placeholder="아이디"
+          />
+          <input
+            type="password"
+            name="user-password"
+            value={password || ""}
+            onChange={onChangePassword}
+            required
+            placeholder="비밀번호"
+          />
+          <button type="submit" className={styles.login_btn}>
+            로그인
+          </button>
+
+          <div className={styles.bottom_option}>
+            <Link href="/find">아이디 / 비밀번호 찾기</Link>
+            <Link href="/signup">회원가입</Link>
+          </div>
+
+          <div className={styles.simple_login}>
+            <h3>간편 로그인</h3>
+            <ul>
+              <li>
+                <Link href={googleLink}>
+                  <figure>
+                    <Image
+                      src={"/img/common/ico_google.png"}
+                      width="42"
+                      height="42"
+                      alt="google"
+                    />
+                  </figure>
+                  <span>Google</span>
+                </Link>
+              </li>
+              <li>
+                <Link href={naverLink}>
+                  <figure>
+                    <Image
+                      src={"/img/common/ico_naver.png"}
+                      width="28"
+                      height="28"
+                      alt="naver"
+                    />
+                  </figure>
+                  <span>네이버</span>
+                </Link>
+              </li>
+              <li>
+                <Link href={kakaoLink}>
+                  <figure>
+                    <Image
+                      src={"/img/common/ico_kakao.png"}
+                      width="42"
+                      height="42"
+                      alt="kakao"
+                    />
+                  </figure>
+                  <span>카카오</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </form>
+      </main>
+    </>
+  );
+}
+export default Login;
---
+++ MonthlyIB/front/components/signUpComponents/SignUp.jsx
@@ -0,0 +1,499 @@
+"use client";
+import { useCallback, useEffect, useRef, useState } from "react";
+import styles from "./Signup.module.css";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
+import { userRegisterWithSocialInfo } from "@/api/userAPI";
+
+import {
+  openAPIVerifyEmail,
+  openAPIVerifyNum,
+  openAPIRegister,
+  openAPIVerifyUsername,
+} from "@/api/openAPI";
+
+const SignUp = () => {
+  const searchParams = useSearchParams();
+  const userId = searchParams.get("id");
+  const paramEmail = searchParams.get("email");
+  const accessToken = searchParams.get("access_token");
+  const authCode = searchParams.get("auth_code");
+  const socialType = searchParams.get("social");
+  const checkBoxes = [
+    {
+      id: "1",
+      name: "firstTerm",
+    },
+    {
+      id: "2",
+      name: "secondTerm",
+    },
+  ];
+
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const pattern = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
+  const [username, setUsername] = useState("");
+  const [checkDuplication, setCheckDuplication] = useState(null);
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [country, setCountry] = useState("");
+  const name = useRef("");
+  const email = useRef("");
+  if (paramEmail) email.current = paramEmail;
+  const dob = useRef("");
+  const school = useRef("");
+  const grade = useRef("");
+  const address = useRef("");
+  const verifyNum = useRef("");
+
+  const [checkedItems, setCheckedItems] = useState([]);
+  const [termError, setTermError] = useState(true);
+  const [consent_marketing, setMarketing] = useState(false);
+
+  const [verifyEmail, setVerifyEmail] = useState(false);
+
+  useEffect(() => {
+    if (session?.userStatus === "ACTIVE") {
+      router.replace("/");
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (checkedItems.length === 2) {
+      setTermError(false);
+    } else {
+      setTermError(true);
+    }
+  }, [checkedItems]);
+
+  const handleSingleCheck = (e, id) => {
+    if (e.target.checked) {
+      setCheckedItems((prev) => [...prev, id]);
+    } else {
+      setCheckedItems(checkedItems.filter((v) => v !== id));
+    }
+  };
+
+  const handleAllCheck = (e) => {
+    if (e.target.checked) {
+      const tempArray = [];
+      checkBoxes.map((v) => tempArray.push(v.id));
+      setCheckedItems(tempArray);
+    } else {
+      setCheckedItems([]);
+    }
+  };
+
+  const onChangeEmail = useCallback((e) => {
+    email.current = e.target.value;
+  }, []);
+  const onChangeVerifyNum = useCallback((e) => {
+    verifyNum.current = e.target.value;
+  }, []);
+
+  const onChangeDob = useCallback((e) => {
+    dob.current = e.target.value;
+  }, []);
+
+  const onChangeSchool = useCallback((e) => {
+    school.current = e.target.value;
+  }, []);
+
+  const onChangeGrade = useCallback((e) => {
+    grade.current = e.target.value;
+  }, []);
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const onChangeAddress = useCallback((e) => {
+    address.current = e.target.value;
+  }, []);
+
+  const onChangeMarketing = useCallback((e) => {
+    setMarketing(e.target.checked);
+  }, []);
+
+  const onChangeName = useCallback((e) => {
+    name.current = e.target.value;
+  }, []);
+
+  const onChangeUsername = useCallback((e) => {
+    setUsername(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+      if (userId) setPasswordError(false);
+    },
+    [password]
+  );
+
+  const onClickVerifyEmail = useCallback(() => {
+    openAPIVerifyEmail(email);
+  }, [email]);
+
+  const onClickVerifyEmailNum = useCallback(async () => {
+    const res = await openAPIVerifyNum(email, verifyNum);
+    if (res?.result.status === 200) setVerifyEmail(true);
+    else if (res?.message === "잘못된 인증 번호 입니다.") {
+      setVerifyEmail(false);
+      alert("잘못된 인증 번호 입니다.");
+    } else {
+      alert("다시 시도해주세요.");
+    }
+  }, [email]);
+
+  const onClickDuplicationCheck = useCallback(async () => {
+    const res = await openAPIVerifyUsername(username);
+    if (res?.result.status === 200) setCheckDuplication(true);
+    else if (res?.message === "USER EXIST") setCheckDuplication(false);
+  }, [username]);
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    if (userId) {
+      const res = await userRegisterWithSocialInfo(
+        userId,
+        accessToken,
+        username,
+        name,
+        dob,
+        school,
+        grade,
+        address,
+        consent_marketing
+      );
+      if (res.result.status === 200) {
+        alert("회원가입이 완료되었습니다. 다시 로그인 해주세요.");
+        router.push("/login");
+      }
+    } else
+      openAPIRegister(
+        username,
+        password,
+        name,
+        email,
+        dob,
+        school,
+        grade,
+        address,
+        country,
+        verifyNum,
+        consent_marketing,
+        signIn
+      );
+  };
+
+  return (
+    <>
+      <main
+        className={`${styles.width_content} ${styles.min_content} ${styles.member}`}
+      >
+        <div className={`${styles.header_tit_wrap} ${styles.tit_center}`}>
+          <span>Member Join</span>
+          <h2>회원가입</h2>
+        </div>
+
+        <form onSubmit={onSubmitForm}>
+          <div className={styles.inputbox_cont}>
+            <input
+              type="text"
+              name="user-id"
+              autoFocus="true"
+              autoComplete="off"
+              required="Y"
+              placeholder="아이디"
+              value={username}
+              onChange={onChangeUsername}
+            />
+            <button type="button" onClick={onClickDuplicationCheck}>
+              중복 확인
+            </button>
+            <div className={styles.frm_msg_cont}>
+              {checkDuplication === null ? (
+                <span className={styles.frm_msg}>
+                  사용하실 아이디를 입력해주세요.
+                </span>
+              ) : checkDuplication === true ? (
+                <span className={`${styles.frm_msg} ${styles.good}`}>
+                  사용가능한 아이디 입니다.
+                </span>
+              ) : (
+                <span className={`${styles.frm_msg} ${styles.frm_msg_war}`}>
+                  중복된 아이디거나 사용할 수 없는 아이디 입니다.
+                </span>
+              )}
+            </div>
+          </div>
+          <div className={styles.inputbox_cont}>
+            <input
+              type="password"
+              maxLength="50"
+              autoComplete="off"
+              required="Y"
+              name="user-pw"
+              placeholder="비밀번호"
+              value={password}
+              onChange={onChangePassword}
+              disabled={userId === null ? false : true}
+            />
+            <div className={styles.frm_msg_cont}>
+              {password === "" || !pattern.test(password) ? (
+                <span className={styles.frm_msg}>
+                  영문, 숫자, 특수문자 조합 8자 이상 입력하세요.
+                </span>
+              ) : (
+                <span className={`${styles.frm_msg} ${styles.good}`}>
+                  사용가능한 비밀번호입니다.
+                </span>
+              )}
+            </div>
+          </div>
+
+          <div className={styles.inputbox_cont}>
+            <input
+              type="password"
+              maxLength="50"
+              autoComplete="off"
+              required="Y"
+              placeholder="비밀번호 확인"
+              name="user-pwcheck"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+              disabled={userId === null ? false : true}
+            />
+            <div className={styles.frm_msg_cont}>
+              {passwordCheck === "" ? (
+                <span className={styles.frm_msg}>
+                  비밀번호를 다시 입력해주세요.
+                </span>
+              ) : passwordError === true ? (
+                <span className={`${styles.frm_msg} ${styles.frm_msg_war}`}>
+                  두 비밀번호가 일치하지 않습니다.
+                </span>
+              ) : (
+                <span className={`${styles.frm_msg} ${styles.good}`}>
+                  비밀번호가 일치합니다.
+                </span>
+              )}
+            </div>
+          </div>
+          <div className={styles.inputbox_cont}>
+            <div className={styles.input_btn_wrap}>
+              <input
+                type="email"
+                name="user-email"
+                placeholder="이메일"
+                defaultValue={email.current}
+                onChange={onChangeEmail}
+                disabled={
+                  paramEmail === null && verifyEmail === false ? false : true
+                }
+              />
+              <button
+                type="button"
+                onClick={onClickVerifyEmail}
+                disabled={
+                  paramEmail === null && verifyEmail === false ? false : true
+                }
+              >
+                인증번호 발송
+              </button>
+            </div>
+          </div>
+          <div className={styles.inputbox_cont}>
+            <div className={styles.input_btn_wrap}>
+              <input
+                type="text"
+                name="user-emailConfirm"
+                placeholder="인증번호"
+                maxLength="6"
+                onChange={onChangeVerifyNum}
+                disabled={
+                  paramEmail === null && verifyEmail === false ? false : true
+                }
+              />
+              <button
+                type="button"
+                onClick={onClickVerifyEmailNum}
+                disabled={
+                  paramEmail === null && verifyEmail === false ? false : true
+                }
+              >
+                확인
+              </button>
+            </div>
+          </div>
+          <div className={styles.inputbox_cont}>
+            <input
+              type="text"
+              name="user-userName"
+              maxLength="50"
+              autoComplete="off"
+              required="Y"
+              placeholder="이름"
+              defaultValue={name.current}
+              onChange={onChangeName}
+            />
+          </div>
+          <div className={styles.inputbox_cont}>
+            <input
+              type="text"
+              name="user-birthDate"
+              defaultValue={dob.current}
+              maxLength="6"
+              autoComplete="off"
+              required="Y"
+              placeholder="생년월일 ( 6자 입력 )"
+              onChange={onChangeDob}
+            />
+          </div>
+          <div className={styles.inputbox_cont}>
+            <input
+              type="text"
+              name="user-school"
+              defaultValue={school.current}
+              maxLength="50"
+              autoComplete="off"
+              required="Y"
+              placeholder="학교"
+              onChange={onChangeSchool}
+            />
+          </div>
+          <div className={styles.inputbox_cont}>
+            <input
+              type="text"
+              name="user-grade"
+              maxLength="50"
+              autoComplete="off"
+              required="Y"
+              placeholder="학년"
+              defaultValue={grade.current}
+              onChange={onChangeGrade}
+            />
+          </div>
+          <div className={styles.inputbox_cont}>
+            <div className={styles.select_cont}>
+              <select className="contry_select" onChange={onChangeCountry}>
+                <option value="">국가선택</option>
+                <option value="ko">한국</option>
+                <option value="en">미국</option>
+                <option value="jp">일본</option>
+                <option value="cn">중국</option>
+              </select>
+              <input
+                type="text"
+                name="user-address"
+                autoComplete="off"
+                required="Y"
+                placeholder="주소"
+                onChange={onChangeAddress}
+              />
+            </div>
+          </div>
+          <div className={styles.frm_agree_cont}>
+            <div className={styles.frm_agree_all}>
+              <input
+                type="checkbox"
+                name="agree_yn_all"
+                onChange={handleAllCheck}
+                checked={
+                  checkedItems.length === checkBoxes.length ? true : false
+                }
+              />
+              <label htmlFor="agree_yn_all">
+                <span></span> 전체동의
+              </label>
+            </div>
+
+            <ul>
+              <li className={styles.frm_agree_auth}>
+                <div className={styles.frm_agree_flex}>
+                  <input
+                    type="checkbox"
+                    name="firstTerm"
+                    onChange={(e) => handleSingleCheck(e, checkBoxes[0].id)}
+                    checked={
+                      checkedItems.includes(checkBoxes[0].id) ? true : false
+                    }
+                    required
+                  />
+                  <label htmlFor="agree_yn1">
+                    <span></span> <b>(필수) </b>이용약관에 동의합니다.
+                  </label>
+                </div>
+                <Link href="/terms" target="_blank">
+                  ( 자세히보기 )
+                </Link>
+              </li>
+              <li className={styles.frm_agree_auth}>
+                <div className={styles.frm_agree_flex}>
+                  <input
+                    type="checkbox"
+                    name="secondTerm"
+                    onChange={(e) => handleSingleCheck(e, checkBoxes[1].id)}
+                    checked={
+                      checkedItems.includes(checkBoxes[1].id) ? true : false
+                    }
+                    required
+                  />
+                  <label htmlFor="agree_yn2">
+                    <span></span> <b>(필수) </b>개인정보 수집 및 이용에
+                    동의합니다.
+                  </label>
+                </div>
+                <Link href="/privacy" target="_blank">
+                  ( 자세히보기 )
+                </Link>
+              </li>
+
+              <li>
+                <div className={styles.frm_agree_flex}>
+                  <input
+                    type="checkbox"
+                    name="consent_marketing"
+                    value={consent_marketing}
+                    onChange={onChangeMarketing}
+                  />
+                  <label htmlFor="marketing_yn">
+                    <span></span> (선택) 커리큘럼, 입시정보, 프로모션 등에 대한
+                    마케팅 메세지 혹은 이메일 수신에 동의합니다
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.center_btn_wrap}>
+            {checkDuplication === true &&
+            passwordError === false &&
+            termError === false ? (
+              // (userId !== null || verifyEmail === true)
+              <button type="submit" className={styles.login_btn}>
+                가입하기
+              </button>
+            ) : (
+              <button disabled={true} className={styles.login_btn}>
+                가입하기
+              </button>
+            )}
+          </div>
+        </form>
+      </main>
+    </>
+  );
+};
+
+export default SignUp;
